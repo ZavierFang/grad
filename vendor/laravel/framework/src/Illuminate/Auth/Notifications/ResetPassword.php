4bddc849d69bc2663b88cf74a6403d4c1ec2c583
zawierfang(方宗伟) <1034052057.com>
2018-04-01 15:54:04 +0800

    first login commit

--- vendor/laravel/framework/src/Illuminate/Auth/Notifications/ResetPassword.php
+++ vendor/laravel/framework/src/Illuminate/Auth/Notifications/ResetPassword.php
@@ -45,8 +45,8 @@ class ResetPassword extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-            ->line('You are receiving this email because we received a password reset request for your account.')
-            ->action('Reset Password', url(config('app.url').route('password.reset', $this->token, false)))
-            ->line('If you did not request a password reset, no further action is required.');
+            ->line('重置密码邮件，你的账号发送重置密码请求，点击重置密码跳转到重置密码链接')
+            ->action('密码重置', url(config('app.url').route('password.reset', $this->token, false)))
+            ->line('如果你没有发送重置密码链接，请不要做任何操作，忽略此邮件');
     }
 }
